7d94f28d502af680d6c1c6e45c097a29d2441e70
Abhijeet Gopalakrishnan <abhijeetgopalakrishnan@gmail.com>
2025-04-03 15:42:18 -0400

    Still have to include to be able to read ZONE, i and j

--- grid_read.cpp
+++ grid_read.cpp
@@ -10,6 +10,11 @@ int main(){
     std::string item;
     char seperator = ',';
     std::vector<double> grid_coords; //Dynamic to append value
+    std::vector<std::vector<double>> twoD_grid_coords;
+    
+    //Initializing sizes nx, ny
+    int nx;
+    int ny;
 
     std::ifstream grid_file("g641x065uf.dat");
     std::cout<<"Reading file"<<std::endl;
@@ -28,6 +33,24 @@ int main(){
     }
 
     //std::cout<<grid_coords[0]<<std::endl<<grid_coords[1]<<std::endl;
+    
+    nx = (int) grid_coords[0];
+    ny = (int) grid_coords[1];   
+    
+    //Running through all coordinates
+    while(std::getline(grid_file, row))
+    {
+        std::vector<double> grid_coords;
+        std::stringstream ss(row);
+        while(std::getline(ss, item, seperator)) grid_coords.push_back(stof(item));
+
+        //Add it to the 2D vector  
+        twoD_grid_coords.push_back(grid_coords);
+
+    }   
+    
+    grid_file.close();
+    std::cout<<"Finished reading file "<<std::endl;
 
     return 0;
 }
