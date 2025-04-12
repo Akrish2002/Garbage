7ef353612356ca24f560c02ce3f6243bd3b224bd
Abhijeet Gopalakrishnan <abhijeetgopalakrishnan@gmail.com>
2025-04-11 20:27:38 -0400

    MUSCL implemented, have to start with fluxlimiter and flux reconstruction

--- 2d_solver/src/schemes/scheme.cpp
+++ 2d_solver/src/schemes/scheme.cpp
@@ -21,34 +21,106 @@ void performMUSCL(
 )
 {
     //Memory allocation
-    allocate(ny - 1, nx - 1, Q);
+    allocate(ny + 1, nx + 1, Q); //Including Halo cells
 
-    allocate(ny - 1, nx, Q_xi_L);
-    allocate(ny - 1, nx, Q_xi_R);
+    allocate(ny + 1, nx + 1, Q_xi_L);
+    allocate(ny + 1, nx + 1, Q_xi_R);
 
-    allocate(ny, nx - 1, Q_eta_L);
-    allocate(ny, nx - 1, Q_eta_R);
+    allocate(ny + 1, nx + 1, Q_eta_L);
+    allocate(ny + 1, nx + 1, Q_eta_R);
 
     std::vector<std::vector<stateVar>> r_R, r_L;
+    
 
     //xi direction
-    //1. First order in the interior cells, since looping from 1 to nx - 1 in xi direction
-    for(size_t i = 0; i < Q[0].size(); i++)
+    //1. Second order in the interior cells
+    for(size_t i = 1; i < ny; i++)
     {
-        for(size_t j = 1; j < Q.size() - 1; j++)
+        //Reasoning for nx - 1 in README, point 3
+        for(size_t j = 2; j < nx; j++)
         {
-            r_R = (Q[i][j + 1] - Q[i][j]) / (Q[i][j + 2] - Q[i][j + 1]); 
-            r_L = (Q[i][j + 1] - Q[i][j]) / (Q[i][j] - Q[i][j - 1]); 
+            /*Shifted the state vectors by -1 to maintain consistency with grid notation for half indices for faces*/
 
-            Q_xi_L = Q[i][j] + 0.25 * epsilon * (Q[i][j] - Q[i][j - 1]) 
-                     * ((1 - kappa) * fluxlimiter(r_L) + (1 + kappa) * r_L * fluxlimiter(1/r_L)); 
+            // r_L = (U_i+1 - U_i) / (U_i - U_i-1)
+            // r_R = (U_i+1 - U_i) / (U_i+2 - U_i+1)
+            r_L = (Q[i][j] - Q[i][j - 1]) / (Q[i][j - 1] - Q[i][j - 2]); 
+            r_R = (Q[i][j] - Q[i][j - 1]) / (Q[i][j + 1] - Q[i][  j  ]); 
+
+
+            // U(^L)_(i+1/2) = U_i   + ε/4 * (U_i   - U_i-1) * [(1 - k) * Φ(r_L) + (1 + k) * r_L * Φ(1/r_L)]
+            // U(^R)_(i+1/2) = U_i+1 + ε/4 * (U_i+2 - U_i+1) * [(1 - k) * Φ(r_R) + (1 + k) * r_R * Φ(1/r_R)]  
+            Q_xi_L[i][j] = Q[i][j - 1]  + 0.25 * epsilon * ( 
+                                           (Q[i][j - 1] - Q[i][j - 2]) * (1.0 - kappa) * fluxlimiter(r_L) 
+                                        +  (Q[i][  j  ] - Q[i][j - 1]) * (1.0 + kappa) * fluxlimiter(1.0/r_L)); 
                 
-            Q_xi_R = Q[i][j] + 0.25 * epsilon * (Q[i][j] - Q[i][j - 1]) 
-                     * ((1 - kappa) * fluxlimiter(r_L) + (1 + kappa) * r_L * fluxlimiter(1/r_L));
-
+            Q_xi_R[i][j] = Q[i][  j  ]  + 0.25 * epsilon * (
+                                           (Q[i][j + 1] - Q[i][  j  ]) * (1.0 - kappa) * fluxlimiter(r_R) 
+                                        +  (Q[i][  j  ] - Q[i][j - 1]) * (1.0 + kappa) * fluxlimiter(1.0/r_L)); 
+            
         }
     }
-                 
+    
+    //2. For the boundary faces
+    for(size_t i = 1; i < ny; i++)
+    {
+        //West Boundary
+        Q_xi_L[i][1] = 0.5*(Q[i][0] + Q[i][1]);
+        Q_xi_R[i][1] = 0.5*(Q[i][0] + Q[i][1]);
+                
+        //East Boundary
+        Q_xi_L[i][nx] = 0.5*(Q[i][nx - 1] + Q[i][nx]);
+        Q_xi_R[i][nx] = 0.5*(Q[i][nx - 1] + Q[i][nx]);
+    }
+
+
+    //eta direction
+    //1. Second order in the interior cells
+    for(size_t i = 1; i < nx; i++)
+    {
+        for(size_t j = 2; j < ny; j++)
+        {
+            /*Shifted the state vectors by -1 to maintain consistency with grid notation for half indices for faces*/
+
+            // r_L = (U_i+1 - U_i) / (U_i - U_i-1)
+            // r_R = (U_i+1 - U_i) / (U_i+2 - U_i+1)
+            r_L = (Q[j][i] - Q[j - 1][i]) / (Q[j - 1][i] - Q[j - 2][i]); 
+            r_R = (Q[j][i] - Q[j - 1][i]) / (Q[j + 1][i] - Q[  j  ][i]); 
+
+
+            // U(^L)_(i+1/2) = U_i   + ε/4 * (U_i   - U_i-1) * [(1 - k) * Φ(r_L) + (1 + k) * r_L * Φ(1/r_L)]
+            // U(^R)_(i+1/2) = U_i+1 + ε/4 * (U_i+2 - U_i+1) * [(1 - k) * Φ(r_R) + (1 + k) * r_R * Φ(1/r_R)]  
+            Q_eta_L[j][i] = Q[j - 1][i]  + 0.25 * epsilon * ( 
+                                           (Q[j - 1][i] - Q[j - 2][i]) * (1.0 - kappa) * fluxlimiter(r_L) 
+                                        +  (Q[  j  ][i] - Q[j - 1][i]) * (1.0 + kappa) * fluxlimiter(1.0/r_L)); 
+                
+            Q_eta_R[j][i] = Q[  j  ][i]  + 0.25 * epsilon * (
+                                           (Q[j + 1][i] - Q[  j  ][i]) * (1.0 - kappa) * fluxlimiter(r_R) 
+                                        +  (Q[  j  ][i] - Q[j - 1][i]) * (1.0 + kappa) * fluxlimiter(1.0/r_L)); 
+            
+        }
+    }
+
+    //2. For the boundary faces
+    for(size_t i = 1; i < nx; i++)
+    {
+        //South Boundary
+        Q_eta_L[1][i] = 0.5*(Q[0][i] + Q[1][i]);
+        Q_eta_R[1][i] = 0.5*(Q[0][i] + Q[1][i]);
+                
+        //North Boundary
+        Q_eta_L[ny][i] = 0.5*(Q[ny - 1][i] + Q[ny][i]);
+        Q_eta_R[ny][i] = 0.5*(Q[ny - 1][i] + Q[ny][i]);
+    }
+
+}
+
+fluxlimiter(std::vector<std::vector<stateVar>>& r)
+{
+
+    
+
+
+
 
 
 }
